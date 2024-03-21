61e379f561894c321c5ea0d53b7fa91cd64f1a6d
Nawdeep Goyal <nawdeep.goyal@target.com>
2024-03-21 22:02:42 +0530

    push to github

---
+++ src/HelloWorld.java
@@ -0,0 +1,36 @@
+public class HelloWorld {
+
+    String name = "nawdeep";
+    int a = 1;
+    short b = 2;
+    boolean g = true;
+    float h = 1.2f;
+    double kajs = 1.5d;
+    char asdhavs = 'a';
+    String t = "shvs";
+
+    int gdsa;
+
+    static String f = "nawdeep";
+
+    public static void main(String[] abcd) {
+        int a = 1;
+        int b = 3;
+        a++; // a = 2
+        a += 1;  //  => a = a +1;
+        int c = a  % b;
+        ++a; // a = 3
+        System.out.println( a != b);
+    }
+
+    int method2(String a){
+        return 1;
+    }
+
+    public void method1() {
+        String c = "nawdeep";
+        method2("1");
+
+        System.out.println(c);
+    }
+}
